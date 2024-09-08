2c8dfd1c8ce02ee11465c95ba73a00ad167bf0c5
Bensoli-Inc <benswambua96@gmail.com>
2024-09-08 06:26:52 +0300

    initial setup

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+// steps to follow:
+// 1.import the package >>> java.sql
+// 2. load and register driver >>> mysql driver>> com.mysql.jdbc.Driver
+// 3. create a connection
+// 4. create a statement
+// 5. execute the query
+// 6. process the results
+// 7. close
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+
+    }
+}
