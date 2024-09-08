c38122de77d362d2d7255acb9edeba46eacc5400
Bensoli-Inc <benswambua96@gmail.com>
2024-09-08 08:14:06 +0300

    configuring readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+installing both mysql server and mysql workbench
+
+creating a database connection in mysql workbench
+creating first database >> abc
+
+using the database >> use abc;
+
+creating first table with 2 columns >> create table student (admissionNo int, studentName varchar(20));
+
+displaying the table >> desc student;
+
+inserting values in the columns >> insert into student values (1001, "Ben");
+ insert into student values (1002, "alex");
+
+displaying the contents of the table >> select * from student;
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,26 @@
 // steps to follow:
 // 1.import the package >>> java.sql
 // 2. load and register driver >>> mysql driver>> com.mysql.jdbc.Driver
-// 3. create a connection
-// 4. create a statement
+// 3. create a connection >>object of connection interface
+// 4. create a statement >>object of statement interface
 // 5. execute the query
 // 6. process the results
 // 7. close
+
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
+        String url = "";
+        String username = "";
+        String password ="";
 
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = getConnection(url, username, password);
+        Statement str;
     }
 }
